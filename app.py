ad48eb8160529399a6d46c790998cd37eedcb7ba
Khushbu Dhiman <dhimankhushbu95@gmail.com>
2026-04-15 17:16:21 +0530

    update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def get_features():
     return jsonify({"features": features})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
